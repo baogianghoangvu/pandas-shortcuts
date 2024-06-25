f49bc2679819681abf561a04b7fabb55486d3ff5
Majestique <nhi.lpu1205@gmail.com>
2024-06-25 21:08:46 +0700

    _wip - revised & updated shortcuts

---
+++ pandas_shortcuts/shortcuts_df_s.py
@@ -0,0 +1,106 @@
+from pandas import DataFrame as D, DataFrameGroupBy as GB, Series as S
+
+# Reindexing / selection / label manipulation
+
+D.f2 = D.rename
+
+## Heads or tails
+D.h = D.head
+S.h = S.head
+
+D.t = D.tail
+S.t = S.tail
+
+
+## Duplicates
+D.dd = D.drop_duplicates
+S.dd = S.drop_duplicates
+
+D.dup = D.duplicated
+S.dup = S.duplicated
+
+
+## Index
+D.sx = D.set_index
+
+D.rx = D.reset_index
+S.rx = S.reset_index
+
+
+# Reshaping, Sorting, Transposing
+
+## Sort
+D.si = D.sort_index
+S.si = S.sort_index
+
+D.sv = D.sort_values
+S.sv = S.sort_values
+
+
+## Pivot
+D.pv = D.pivot
+
+D.pvt = D.pivot_table
+
+
+# Groupby
+D.gb = D.groupby
+S.gb = S.groupby
+
+
+# Missing data handling
+D.dna = D.dropna
+S.dna = S.dropna
+
+D.fna = D.fillna
+S.fna = S.fillna
+
+
+# Computations / descriptive stats
+D.desc = D.describe
+
+D.vc = D.values_count
+S.vc = S.values_count
+
+D.nu = D.nunique
+S.nu = S.nunique
+
+S.u = S.unique
+
+
+# Properties
+D.c = D.columns
+
+D.i = D.index
+S.i = S.index
+
+
+# IO
+D.cb = D.to_clipboard
+S.cb = S.to_clipboard
+
+D.dict = D.to_dict
+S.dict = S.to_dict
+
+S.list = S.to_list
+
+D.np = D.to_numpy
+S.np = S.to_numpy
+
+## File types
+D.csv = D.to_csv
+S.csv = S.to_csv
+
+D.html = D.to_html
+
+D.json = D.to_json
+S.json = S.to_json
+
+D.md = D.to_markdown
+S.md = S.to_markdown
+
+D.parquet = D.to_parquet
+S.parquet = S.to_parquet
+
+D.xlsx = D.to_excel
+S.xlsx = S.to_excel
---
+++ pandas_shortcuts/shortcuts_pd.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+# IO
+pd.csv = pd.read_csv
+pd.json = pd.read_json
+pd.parquet = pd.read_parquet
+pd.sql = pd.read_sql
+pd.xlsx = pd.read_excel
+
+
+# General function - Pivot
+pd.pv = pd.pivot
+pd.pvt = pd.pivot_table
+
+
+# General function - datetime
+pd.tdt = pd.to_datetime
+pd.ttd = pd.to_timedelta
